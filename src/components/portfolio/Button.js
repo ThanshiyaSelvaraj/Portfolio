38ea82e8658b5889c4f2b990bef1a442580c82a1
ThanshiyaSelvaraj <163156402+ThanshiyaSelvaraj@users.noreply.github.com>
2024-09-20 10:22:55 +0530

    Initial commit

---
+++ src/components/portfolio/Button.js
@@ -0,0 +1,11 @@
+function Button(){
+    return(
+        <div className="button">
+      <button>About</button>
+      <button>Skills</button>
+      <button>Experience</button>
+      <button>Schedule call</button>
+      </div>
+    )
+}
+export default Button;
---
+++ src/components/portfolio/Paragraph.js
@@ -0,0 +1,8 @@
+function Paragraph(){
+    return(
+        <p>I am currently enrolled as a senior in the College of Engineering at Anna University . 
+            I am majoring in Computer Science and Engineering Department . 
+            I am interested on Cybersecurity and web developing field and I am seeking exciting opportunities related to the same.</p>
+    )
+}
+export default Paragraph;
